8e7f9d274f3eaa2848c05fd3cdff34258b2f036a
Farizal Hanafi <102751082+hunter934@users.noreply.github.com>
2025-04-14 12:27:11 +0700

    Update Epl_streamlit.py

--- pages/Epl_streamlit.py
+++ pages/Epl_streamlit.py
@@ -29,8 +29,9 @@ if left.button("START", use_container_width=True, type='primary'):
     roll_df = sort_data(df)
     roll_df = encode_data(roll_df)
     roll_df_normal = normalization_data(roll_df)
-    
-if right.button("See the Data", use_container_width=True, type='secondary'):
+
+page_1 = st.page_link("pages/1_See_The_Data.py")
+right.button("See the Data", use_container_width=True, type='secondary', on_click=page_1)
 
 home_team = st.selectbox("Choose Home Team",
                          ('Manchester Utd', 'Ipswich Town', 'Newcastle Utd', "Nott'ham Forest",
