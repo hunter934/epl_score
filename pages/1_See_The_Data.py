b489e3fd146619fad75c53d66f77644a6dd7c343
Farizal Hanafi <102751082+hunter934@users.noreply.github.com>
2025-04-15 13:25:06 +0700

    Update 1_See_The_Data.py

--- pages/1_See_The_Data.py
+++ pages/1_See_The_Data.py
@@ -2,8 +2,6 @@ import streamlit as st
 import numpy as np
 from function_epl import *
 
-st.set_page_config(page_title='EPL Database', page_icon='⚽')
-
 st.header('EPL Database')
 st.dataframe(df)
 st.caption('Last update: 20-03-2025')
