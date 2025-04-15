19d6bde133d5697a6aff2188fe7f53867c846132
Farizal Hanafi <102751082+hunter934@users.noreply.github.com>
2025-04-15 14:22:35 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -20,7 +20,9 @@ st.write('English Premier League Version. This is for FUN only')
 df = read_data('epl-2017-2025-03-20.csv')
 
 left, right = st.columns(2)
-left.button("START", use_container_width=True, type='primary', on_click=page_link("pages/2_Predict_Score.py"))
-right.button("See the Data", use_container_width=True, type='secondary', on_click=page_link("pages/1_See_The_Data.py"))
+with left:
+    st.link_button("START", use_container_width=True, type='primary', st.page_link("pages/2_Predict_Score.py"))
+with right:
+    st.link_button("See the Data", use_container_width=True, type='secondary', st.page_link("pages/1_See_The_Data.py"))
 
 st.caption("Last Update: 20-03-2025")
